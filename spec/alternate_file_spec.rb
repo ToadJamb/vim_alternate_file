efda47b0a2670781def0177bcd38dbee1d417b6a
Travis Herrick <tthetoad@gmail.com>
2017-04-06 03:36:42 -0700

    wip

--- spec/alternate_file_spec.rb
+++ spec/alternate_file_spec.rb
@@ -1,50 +1,133 @@
 require 'spec_helper'
 require 'fileutils'
 
-RSpec.describe 'VimAlternateFile' do
-  def run_commands(*commands)
+RSpec.shared_context 'plain', :plain do
+  let(:command) { "\\\"#{fx}\\\"" }
+end
+
+RSpec.shared_context 'vim', :vim do
+  subject { output.first }
+
+  let(:command) { "sid . \\\"#{fx}(#{fx_args})\\\"" }
+  let (:output) { [] }
+  let(:verbose) { true }
+  let(:args)    { '' }
+
+  def run_command
+    FileUtils.rm 'spec/out.txt' if File.file?('spec/out.txt')
+
+    puts cmd if verbose
+    system cmd
+
+    File.open('spec/out.txt') do |file|
+      file.each_line.with_index do |line, i|
+        next unless i > 1
+
+        val = eval(line.chomp)
+        #val = val.to_i if val.to_i.to_s == val
+        #val = val.to_f if val.to_f.to_s == val
+
+        output << val
+      end
+    end
+    FileUtils.rm 'spec/out.txt' if File.file?('spec/out.txt')
+
+    if verbose
+      puts '-' * 80
+      puts output
+      puts '-' * 80
+    end
+
+    msg = "expected exactly one line (got #{output.count}):\n" +
+      cmd + "\n" +
+      '-' * 80 + "\n" +
+      output.join("\n")
+    expect(output.count).to eq(1), msg
+  end
+
+  private
+
+  def cmd
+    return @cmd if defined?(@cmd)
+
+    subs =
+      if command.match(/\%s/)
+        command % args
+      else
+        command
+      end
+
     cmds = [
       'redir @a',
-      *commands,
+      'let sid = g:alternate_file_sid',
+      "let command = #{subs}",
+      "execute 'let val = ' . command",
+      "echo val",
       'put = @a',
-      'w! >> out.txt',
+      'w! >> spec/out.txt',
       'qall!',
     ].flatten.map do |cmd|
       "\"+#{cmd}\""
     end.join ' '
 
-    command = "vi -u spec/.vimrc #{cmds}"
-    puts command
+    @cmd = "vi -u spec/.vimrc #{cmds}"
+  end
+end
 
-    `#{command}`
+RSpec.configure do |rspec|
+  rspec.include_context 'vim', :vim
+  rspec.include_context 'plain', :plain
+end
 
-    File.open('out.txt') do |file|
-      file.each_line.with_index do |line, i|
-        next unless i > 1
-        output << line
+RSpec.describe 'VimAlternateFile', :vim do
+  describe '.is_spec_folder' do
+    let(:fx)      { 'is_spec_folder' }
+    let(:fx_args) { "'%s'" }
+
+    context 'relies on globpath output', :plain do
+      let(:fx)  { "split(globpath(getcwd(), '*/'), '\n')" }
+
+      before { run_command }
+
+      it 'returns paths with a trailing slash' do
+        expect(subject.count).to be > 1
+        subject.each do |path|
+          expect(path).to match(/\/$/)
+        end
       end
     end
-  end
 
-  let (:output) { [] }
+    shared_examples 'is_spec_folder' do |path, expected|
+      let(:args) { path }
 
-  before { FileUtils.rm 'out.txt' if File.file?('out.txt') }
-  after { FileUtils.rm 'out.txt' if File.file?('out.txt') }
+      context "given a path of #{path.inspect}" do
+        before { run_command }
 
-  it 'works' do
-    run_commands(
-      "echo strftime('%c')",
-      "echo strftime('%c')",
-      "echo strftime('%c')",
-    )
-    puts '-' * 80
-    puts output
-    puts '-' * 80
+        it "returns #{expected.inspect}" do
+          expect(subject).to eq expected
+        end
+      end
+    end
 
-    expect(true).to eq true
-  end
+    it_behaves_like 'is_spec_folder', '/spec/', 1
+    it_behaves_like 'is_spec_folder', 'x/spec/', 1
+    it_behaves_like 'is_spec_folder', '/spec/x', 0
+    it_behaves_like 'is_spec_folder', '/xspec/', 0
+    it_behaves_like 'is_spec_folder', '/specx/', 0
 
-  it 'maybe works' do
-    expect(true).to eq true
+
+
+
+
+
+
+
+    #it_behaves_like 'is_spec_folder', '/spec', 0
+    #it_behaves_like 'is_spec_folder', 'spec', 0
+    #it_behaves_like 'is_spec_folder', 'spec/', 0
+
+
+    #it_behaves_like 'is_spec_folder', '/xspec/', 0
+    #it_behaves_like 'is_spec_folder', '/specx/', 0
   end
 end
